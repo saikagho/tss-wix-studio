7b6dd3583a9150d548d3c95c9a69d1a3c85890cb
SAIKAT GHOSH <34501686+saikagho@users.noreply.github.com>
2024-01-30 19:54:07 +0100

    Add files via upload

---
+++ typeform-hidden-fields-update.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+       const gclid = "gclid";
+       const gClientId = "test_ga_client_id";
+       const gMedium = "test_ga_medium";
+       const gSource = "test_ga_source";
+       const landingPage = "https://www.mescoursparticuliers.fr/";	
+       const cookieString = `gclid=${gclid},ga_client_id=${gClientId},ga_medium=${gMedium},ga_source=${gSource},landing_page_url=${landingPage}`
+
+	   const typeform = document.getElementById("js-typeform-update");
+       typeform.removeAttribute("data-tf-hidden");
+	   console.log(typeform, cookieString)
+       typeform.setAttribute("data-tf-hidden", cookieString);
+});
