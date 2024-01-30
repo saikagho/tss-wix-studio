b32c23e831e7fd475d4000b3038a383f16134059
SAIKAT GHOSH <34501686+saikagho@users.noreply.github.com>
2024-01-30 20:20:49 +0100

    Update typeform-hidden-fields-update.js

--- typeform-hidden-fields-update.js
+++ typeform-hidden-fields-update.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
        const landingPage = "https://www.mescoursparticuliers.fr/";	
        const cookieString = `gclid=${gclid},ga_client_id=${gClientId},ga_medium=${gMedium},ga_source=${gSource},landing_page_url=${landingPage}`
 
-	   const typeform = document.getElementById("js-typeform-update");
+       const typeform = document.getElementById("js-typeform-update");
        typeform.removeAttribute("data-tf-hidden");
-	   console.log(typeform, cookieString)
+       console.log(typeform.getAttribute("data-tf-hidden"), cookieString)
        typeform.setAttribute("data-tf-hidden", cookieString);
 });
